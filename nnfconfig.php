96726de05b23cea2b6ce20b936e6adc17730e3fc
Ertuğrul TURAN <60829297+obirninja@users.noreply.github.com>
2020-04-04 23:46:18 +0300

    Update nnfconfig.php

--- nnfconfig.php
+++ nnfconfig.php
@@ -1,5 +1,5 @@
 <?php
-//YoutWeb site domain (url) - Not: https://site.com/ Use: https://site.com
+//Your Web site domain (url) - Not: https://site.com/ Use: https://site.com
 $mywebsiteurlx = "https://site.com";
 //CloudFlare Api Connect;
 //X-Auth-Email & X-Auth-Key
